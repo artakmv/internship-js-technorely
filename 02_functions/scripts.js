b50d6ef0593fe83927d1799edc7d3fee491bf696
artakmanukian <artakmv@icloud.com>
2021-09-21 15:19:35 +0300

    solved third task

--- 02_functions/scripts.js
+++ 02_functions/scripts.js
@@ -1,5 +1,6 @@
 let arr1 = [1, 2, 3, 3, 'a', 'test']
 let arr2 = ['test', 2, 7,  'a', 'a']
+let arr3 = [111896, 9999, 9985, 1024, 999999, 1000100, 60044943, 12135000000]
 
 // More used element in arrays function
 const moreUsedEl = (arr1, arr2) => {
@@ -25,3 +26,30 @@ const removeDuplicates = (array) => {
 }
 
 console.log(removeDuplicates(arr1))
+
+// Abbreviated numbers
+const abb = (arr) => {
+  const abbreviate = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "K" },
+    { value: 999999, symbol: "M" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "B" }
+  ]
+  const result = []
+
+  for (let el of arr) {
+    // Get correct abbreviate for element
+    let item = abbreviate.slice().reverse().find((item) => {
+      return el >= item.value
+    })
+
+    // Transform number from array(element) to Abbreviated number
+    let abbNum = item ? (Math.round((el / item.value) * 100) / 100) + item.symbol : '0'
+    result.push(abbNum)
+  }
+
+  return result
+}
+
+console.log(abb(arr3))
